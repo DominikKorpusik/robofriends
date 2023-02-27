7f9c688bed943a8d6390dd3828be15b594ec49cf
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-02-27 22:40:57 +0000

    Adding hello.js

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div className="Hello">
+                <header className="Hello-header">
+                    <h1>Hello World</h1>
+                    <p>lorem</p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Hello;
