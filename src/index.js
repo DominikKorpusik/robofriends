b37b4880096bc67a2b21b4d8fbf141757633f1a4
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-02-27 23:07:16 +0000

    Adding Card

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import Card from './Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Card />
   </React.StrictMode>
 );
 
