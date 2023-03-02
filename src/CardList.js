3027d87b1cebee490e6e87ee22b86bd6829778f0
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-03-02 21:46:04 +0000

    Making CardList

---
+++ src/CardList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from './Card';
+
+class CardList extends React.Component {
+    render() {
+        const { robots } = this.props;
+        const cardsArray = robots.map((user) => {
+            return (
+                <Card
+                    key={user.id}
+                    id={user.id}
+                    name={user.name}
+                    email={user.email}
+                />
+            );
+        })
+        return (
+            <div>
+                {cardsArray}
+            </div>
+        );
+    }
+}
+
+export default CardList
